bf5f6eb196e1e10287084671b3606fb8060200c4
wlf <wlf@qq.com>
2022-03-26 06:32:29 -0700

    ,

--- src/server.cpp
+++ src/server.cpp
@@ -14,14 +14,10 @@ long isplit(string str){
     int i = 3,pos = 0;
     long num = 0;
     while(i){
-        cout<<str<<endl;
         pos = str.find('.');
-        cout<<pos<<endl;
-        cout<<str.substr(0,pos)<<endl;
         num = pow(10,pos)*num + stoi(str.substr(0,pos));
         str = str.substr(pos+1);
         i--;
-        cout<<num<<endl;
     }
     num = pow(10,str.size())*num + stoi(str);
     return num;
